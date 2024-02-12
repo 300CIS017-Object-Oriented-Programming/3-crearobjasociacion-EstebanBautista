e3e06f5d0640d7599ec5226faa1559f873f85c44
EstebanBautista <124476884+EstebanBautista@users.noreply.github.com>
2024-02-11 21:50:23 -0500

    Update Propietario.cpp

--- Propietario.cpp
+++ Propietario.cpp
@@ -5,7 +5,10 @@
 #include "Propietario.h"
 
 //TODO Agrege el constructor por defecto y agrege un constructor con dos parametros
-
+Propietario::Propietario(std::string nombre) {
+  this->nombre = nombre;
+  this->docIdentidad = "";
+}
 
 Propietario::Propietario(std::string nombre, std::string docIdentidad) {
  this->nombre = nombre;
@@ -18,4 +21,10 @@ std::string Propietario::getNombre()  {
 
 void Propietario::setNombre(std::string nombre) {
     this->nombre = nombre;
+}
+
+void Propietario::mostrarInfo() {
+    std::cout << "Nombre: " << nombre << std::endl;
+    std::cout << "Documento de Identidad: " << docIdentidad << std::endl;
+    std::cout << "Edad: " << edad << std::endl;
 }
