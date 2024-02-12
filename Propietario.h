45614f3ec44e1dfe0c2bcda25912f246a9b8f4a2
EstebanBautista <124476884+EstebanBautista@users.noreply.github.com>
2024-02-11 21:50:29 -0500

    Update Propietario.h

--- Propietario.h
+++ Propietario.h
@@ -15,10 +15,20 @@ private:
 public:
     Propietario() = default; //Agrega constructor por defecto sin cuerpo
     Propietario(std::string nombre, std::string docIdentidad);
+    Propietario(std::string nombre);
     void mostrarInfo();
     std::string getNombre();
     void setNombre(std::string nombre);
-
+    std::string getDocIdentidad();
+    void setDocIdentidad(std::string docIdentidad);
+    int getEdad();
+    void setEdad(int edad);
+    
+/*
+El metodo "agregarPropietario" se supone que es lo que conecta a la clase perro y la clase propietario
+este método crea una relación entre un perro y su propietario asignando un objeto Propietario al perro. De esta manera, 
+establece una asociación entre un perro y su propietario, lo que permite que el perro tenga un propietario específico.
+*/
 
 
 };
